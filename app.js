4a99bead2f17c35999dcaeacfed6be86f47b9a5b
Sai M <71229141+Matta-Sai@users.noreply.github.com>
2024-10-03 10:54:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let useridInput = document.getElementById("userId")
 let submitButton = document.getElementById("clickHereButton")
-const urlStart = "http://appsdms75.maruti.com:7779/forms/frmservlet?config=ggsmcnnoc1&p_user_id="
+const urlStart = "http://appsdms81.maruti.com:7779/forms/frmservlet?config=ggsmcnnoc1&p_user_id="
 const urlCenter = "&p_sid="
 const urlEnd = "&p_pmc=1"
 
@@ -16,7 +16,7 @@ function onClickSubmit() {
     let month = today.getMonth() + 1
     let date = today.getDate()
     month = month.toString()
-    month = "0" + month
+    month.length === 1 ? month = "0" + month : month = month
     year = year.substring(2)
 
     window.open(`${urlStart}${useridInput.value}${urlCenter}${minutes}${hours}${year}${month}${date}${urlEnd}`)
